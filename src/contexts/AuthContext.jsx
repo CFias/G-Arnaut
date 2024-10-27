99dbd50c0713d4e896c6e72f2111d1627dbbf443
Cleidson Ramos Fias <116103790+CFias@users.noreply.github.com>
2024-10-27 16:50:51 -0300

    feat: edit Banner Img

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,8 +1,7 @@
-// src/contexts/AuthContext.jsx
 import React, { createContext, useContext, useState, useEffect } from "react";
 import { onAuthStateChanged } from "firebase/auth";
-import { auth, db } from "../services/FirebaseConfig"; // Importando db corretamente
-import { doc, getDoc } from "firebase/firestore"; // Importando métodos do Firestore
+import { auth, db } from "../services/FirebaseConfig";
+import { doc, getDoc } from "firebase/firestore";
 
 const AuthContext = createContext();
 
@@ -17,13 +16,11 @@ export function AuthProvider({ children }) {
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, async (user) => {
       if (user) {
-        // Se o usuário estiver autenticado, busque o nome do usuário
-        const userDoc = await getDoc(doc(db, "users", user.uid)); // Ajuste o caminho da coleção conforme sua estrutura
+        const userDoc = await getDoc(doc(db, "users", user.uid));
         if (userDoc.exists()) {
-          // Verifica se o documento existe
-          setCurrentUser({ ...user, userName: userDoc.data().username }); // Supondo que o campo username exista no Firestore
+          setCurrentUser({ ...user, userName: userDoc.data().username });
         } else {
-          setCurrentUser({ ...user, userName: "Usuário sem nome" }); // Lida com a ausência de nome
+          setCurrentUser({ ...user, userName: "Usuário sem nome" });
         }
       } else {
         setCurrentUser(null);
@@ -34,7 +31,7 @@ export function AuthProvider({ children }) {
     return unsubscribe;
   }, []);
 
-  const value = { currentUser, loading }; // Adicione o estado de carregamento aqui
+  const value = { currentUser, loading };
 
   return (
     <AuthContext.Provider value={value}>
